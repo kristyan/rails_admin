0ec564819061cf063c65fc554270f4c20282c227
Kristyan <kristyan@teamer.net>
2011-04-01 14:20:39 +0100

    removed expnsive counts

--- app/controllers/rails_admin/main_controller.rb
+++ app/controllers/rails_admin/main_controller.rb
@@ -22,7 +22,7 @@ module RailsAdmin
       @count = {}
       @max = 0
       @abstract_models.each do |t|
-        current_count = 0 #t.count
+        current_count = t.count
         @max = current_count > @max ? current_count : @max
         @count[t.pretty_name] = current_count
         @most_recent_changes[t.pretty_name] = AbstractHistory.most_recent_history(t.pretty_name).last.try(:updated_at)
